3dea4ab59b9661e7873a140375a8b03b785d05dc
salihamad <samy_alihamad@yahoo.com>
2014-08-17 11:18:17 -0500

    Added new theme. Added TalkClient.jar reference to send messages between mobile and wear

--- mobile/src/main/java/com/hhtrashservices/www/hhtrashservices/MainActivity.java
+++ mobile/src/main/java/com/hhtrashservices/www/hhtrashservices/MainActivity.java
@@ -1,27 +1,31 @@
 package com.hhtrashservices.www.hhtrashservices;
 
-import java.util.Locale;
-
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.provider.Settings;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
 import com.parse.ParseUser;
 
+import se.heinrisch.talkclient.TalkClient;
+import se.heinrisch.talkclient.adapters.TalkMessageAdapter;
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -33,15 +37,21 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
 
+    private TalkClient mTalkClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        mTalkClient = new TalkClient(this);
 
         ParseUser currentUser = ParseUser.getCurrentUser();
 
@@ -54,7 +64,8 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        //mSectionsPagerAdapter = new SectionsPagerAdapter(this,getFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(this,getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -81,6 +92,32 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
         }
+
+        sendMessage();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+ //       mTalkClient.connectClient();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mTalkClient.disconnectClient();
+        super.onDestroy();
+    }
+
+    private void sendMessage() {
+        DataMap dataMap = new DataMap();
+        dataMap.putString("data", "HH Trash Services Says Hello");
+        mTalkClient.setTalkMessageAdapter(new TalkMessageAdapter(){
+            @Override
+            public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                Log.e(TAG, "Message sent");
+            }
+        });
+        mTalkClient.sendMessage("/my-path", dataMap);
     }
 
     private void navigateToLogin() {
@@ -100,8 +137,13 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
             ParseUser.logOut();
             navigateToLogin();
         }
+        else if(itemId  == R.id.action_apartment){
+            Intent intent = new Intent(this, SelectApartmentActivity.class);
+            startActivity(intent);
+        }
         return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -123,44 +165,6 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
     }
 
     /**
@@ -191,7 +195,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
 
